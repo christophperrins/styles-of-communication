80c2305957e6f7c9b54ebe45f85af94211146d49
Chris Perrins <chrisperrins95@gmail.com>
2020-05-13 00:50:08 +0100

    added charts, finished questions, added progress bar and how to communicate with different individuals - needs HEAVY refactoring

--- main.js
+++ main.js
@@ -92,90 +92,120 @@ let questions = (() => {
       value: "I listen to people", action: () => communicationStyles.addToPeople()
     }, {
       value: "People say that I am a fast thinker", action: () => communicationStyles.addToIdeas()
+    }], [{
+      value: "Cooperation is a key word for me", action: () => communicationStyles.addToPeople()
+    }, {
+      value: "I use logical methods to test alternatives", action: () => communicationStyles.addToProcess()
+    }], [{
+      value: "I like to handle several projects at the same time", action: () => communicationStyles.addToAction()
+    }, {
+      value: "I always question myself", action: () => communicationStyles.addToProcess()
+    }], [{
+      value: "I learn by doing", action: () => communicationStyles.addToAction()
+    }, {
+      value: "I believe that my head rules my heart", action: () => communicationStyles.addToProcess()
+    }], [{
+      value: "I can predict how others may react to a certain action", action: () => communicationStyles.addToPeople()
+    }, {
+      value: "I do not like details", action: () => communicationStyles.addToIdeas()
+    }], [{
+      value: "Analysis should always precede action", action: () => communicationStyles.addToProcess()
+    }, {
+      value: "I am able to assess the climate of a group", action: () => communicationStyles.addToPeople()
+    }], [{
+      value: "I have a tendency to start things and not finish them up", action: () => communicationStyles.addToIdeas()
+    }, {
+      value: "I perceive myself as decisive", action: () => communicationStyles.addToAction()
+    }], [{
+      value: "I search for challenging tasks", action: () => communicationStyles.addToAction()
+    }, {
+      value: "I rely on observation and data", action: () => communicationStyles.addToProcess()
+    }], [{
+      value: "I can express my feelings openly", action: () => communicationStyles.addToPeople()
+    }, {
+      value: "I like to design new projects", action: () => communicationStyles.addToIdeas()
+    }], [{
+      value: "I enjoy reading very much", action: () => communicationStyles.addToIdeas()
+    }, {
+      value: "I perceive myself as a facilitator", action: () => communicationStyles.addToPeople()
+    }], [{
+      value: "I like to focus on one issue at a time", action: () => communicationStyles.addToProcess()
+    }, {
+      value: "I like to achieve", action: () => communicationStyles.addToAction()
+    }], [{
+      value: "I enjoy learning about others", action: () => communicationStyles.addToPeople()
+    }, {
+      value: "I like variety", action: () => communicationStyles.addToAction()
+    }], [{
+      value: "Facts speak for themselves", action: () => communicationStyles.addToProcess()
+    }, {
+      value: "I use my imagination as much as possible", action: () => communicationStyles.addToIdeas()
+    }], [{
+      value: "I am impatient with long, slow assignments", action: () => communicationStyles.addToAction()
+    }, {
+      value: "My mind never stops working", action: () => communicationStyles.addToIdeas()
+    }], [{
+      value: "Key decisions have to be made in a cautious way", action: () => communicationStyles.addToProcess()
+    }, {
+      value: "I strongly believe that people need each other to get work done", action: () => communicationStyles.addToPeople()
+    }], [{
+      value: "I usually make decisions without thinking too much", action: () => communicationStyles.addToAction()
+    }, {
+      value: "Emotions create problems", action: () => communicationStyles.addToProcess()
+    }], [{
+      value: "I like to be liked by others", action: () => communicationStyles.addToPeople()
+    }, {
+      value: "I can put two and two together very quickly", action: () => communicationStyles.addToIdeas()
+    }], [{
+      value: "I like to be liked by others", action: () => communicationStyles.addToPeople()
+    }, {
+      value: "I can put two and two together very quickly", action: () => communicationStyles.addToIdeas()
+    }], [{
+      value: "I try out my new ideas on people", action: () => communicationStyles.addToIdeas()
+    }, {
+      value: "I believe in the scientific approach", action: () => communicationStyles.addToProcess()
+    }], [{
+      value: "I like to get things done", action: () => communicationStyles.addToAction()
+    }, {
+      value: "Good relationships are essential", action: () => communicationStyles.addToPeople()
+    }], [{
+      value: "I am impulsive", action: () => communicationStyles.addToAction()
+    }, {
+      value: "I accept differences in people", action: () => communicationStyles.addToProcess()
+    }], [{
+      value: "Communicating with people is an end in itself", action: () => communicationStyles.addToPeople()
+    }, {
+      value: "I like to be intellectually stimulated", action: () => communicationStyles.addToIdeas()
+    }], [{
+      value: "I like to organize", action: () => communicationStyles.addToProcess()
+    }, {
+      value: "I usually jump from one task to another", action: () => communicationStyles.addToAction()
+    }], [{
+      value: "Talking and working with people is a creative art", action: () => communicationStyles.addToPeople()
+    }, {
+      value: "Self-actualization is a key word for me", action: () => communicationStyles.addToIdeas()
+    }], [{
+      value: "I enjoy playing with ideas", action: () => communicationStyles.addToIdeas()
+    }, {
+      value: "I dislike to waste my time", action: () => communicationStyles.addToAction()
+    }], [{
+      value: "I enjoy doing what I am good at", action: () => communicationStyles.addToProcess()
+    }, {
+      value: "I learn by interacting with others", action: () => communicationStyles.addToPeople()
+    }], [{
+      value: "I find abstractions interesting and enjoyable", action: () => communicationStyles.addToIdeas()
+    }, {
+      value: "I am patient with details", action: () => communicationStyles.addToProcess()
+    }], [{
+      value: "I like brief, to the point statements", action: () => communicationStyles.addToAction()
+    }, {
+      value: "I feel confident in myself", action: () => communicationStyles.addToPeople()
     }]
   ]
 
-// "Cooperation is a key word for me"
-// "I use logical methods to test alternatives"
-
-// "I like to handle several projects at the same time"
-// "I always question myself"
-
-// "I learn by doing"
-// "I believe that my head rules my heart"
-
-// "I can predict how others may react to a certain action"
-// "I do not like details"
-
-// "Analysis should always precede action"
-// "I am able to assess the climate of a group"
-
-// "I have a tendency to start things and not finish them up"
-// "I perceive myself as decisive"
-
-// "I search for challenging tasks"
-// "I rely on observation and data"
-
-// "I can express my feelings openly"
-// "I like to design new projects"
-
-// "I enjoy reading very much"
-// "I perceive myself as a facilitator"
-
-// "I like to focus on one issue at a time"
-// "I like to achieve"
-
-// "I enjoy learning about others"
-// "I like variety"
-
-// "Facts speak for themselves"
-// "I use my imagination as much as possible"
-
-// "I am impatient with long, slow assignments"
-// "My mind never stops working"
-
-// "Key decisions have to be made in a cautious way"
-// "I strongly believe that people need each other to get work done"
-
-// "I usually make decisions without thinking too much"
-// "Emotions create problems"
-
-// "I like to be liked by others"
-// "I can put two and two together very quickly"
-
-// "I try out my new ideas on people"
-// "I believe in the scientific approach"
-
-// "I like to get things done"
-// "Good relationships are essential"
-
-// "I am impulsive"
-// "I accept differences in people"
-
-// "Communicating with people is an end in itself"
-// "I like to be intellectually stimulated.
-
-// "I like to organize"
-// "I usually jump from one task to another"
-
-// "Talking and working with people is a creative art"
-// "Self-actualization is a key word for me"
-
-// "I enjoy playing with ideas"
-// "I dislike to waste my time"
-
-// "I enjoy doing what I am good at"
-// "I learn by interacting with others"
-
-// "I find abstractions interesting and enjoyable"
-// "I am patient with details"
-
-// "I like brief, to the point statements"
-// "I feel confident in myself"
-
   return {
-    next: () => index = index+1,
-    getPercentageOfComplete: () => (index+1)/questions.length*100,
+    next: () => index = index + 1,
+    getPercentageOfComplete: () => index / questions.length * 100,
     getNumberOfQuestions: () => questions.length,
     getNumberOfOptions: () => questions[index] ? questions[index].length : 0,
     getOption: (location) => questions[index][location],
@@ -183,9 +213,9 @@ let questions = (() => {
 })();
 
 let choices = (() => {
-  
-  let colour = (i) => i%2==0 ? "btn-primary" : "btn-secondary";
-  
+
+  let colour = (i) => i % 2 == 0 ? "btn-primary" : "btn-secondary";
+
   let choicesRow = document.getElementById("choices");
   choicesRow.innerHTML = "";
 
@@ -210,10 +240,10 @@ let choices = (() => {
     let col = document.createElement("div")
     col.className = "col-12";
     let progressContainer = document.createElement("div");
-    progressContainer.className="progress my-2";
+    progressContainer.className = "progress my-2";
     let progressBar = document.createElement("div");
-    progressBar.className= "progress-bar progress-bar-striped";
-    progressBar.setAttribute("style", "width: " + parseInt(questions.getPercentageOfComplete())+"%");
+    progressBar.className = "progress-bar progress-bar-striped";
+    progressBar.setAttribute("style", "width: " + parseInt(questions.getPercentageOfComplete()) + "%");
     progressContainer.append(progressBar);
     col.append(progressContainer);
 
@@ -223,35 +253,169 @@ let choices = (() => {
   let displayResults = () => {
     let row = document.getElementById("choices")
     row.id = "results";
-    let col = document.createElement("div");
-    col.className = "col-12";
+    let results = document.createElement("div");
+    results.className = "col-lg-6 col-sm-12";
     let heading = document.createElement("h3");
     heading.innerText = "Your results:"
-    let list = document.createElement("ul");
-    let actions = document.createElement("li");
-    actions.innerText = "Actions: " + parseInt(communicationStyles.getAction() / questions.getNumberOfQuestions() * 100) + "%";
-    let process = document.createElement("li");
-    process.innerText = "Process: " + parseInt(communicationStyles.getProcess() / questions.getNumberOfQuestions() * 100) + "%";
-    let people = document.createElement("li");
-    people.innerText = "People: " + parseInt(communicationStyles.getPeople() / questions.getNumberOfQuestions() * 100) + "%";
-    let ideas = document.createElement("li");
-    ideas.innerText = "Ideas: " + parseInt(communicationStyles.getIdeas() / questions.getNumberOfQuestions() * 100) + "%";
-  
-    list.append(actions);
-    list.append(process);
-    list.append(people);
-    list.append(ideas);
+    let character = document.createElement("h4");
+
+    let orientation = "Action";
+    let value = communicationStyles.getAction();
+    if (communicationStyles.getProcess() > value) {
+      orientation = "Process"
+    }
+    if (communicationStyles.getPeople() > value) {
+      orientation = "People"
+    }
+    if (communicationStyles.getIdeas() > value) {
+      orientation = "Ideas"
+    }
+    character.innerHTML = "You are a <b>" + orientation + "</b> orientated individual";
+
+    let orientationButton = (() => {
+      let numberCreated = 0;
+      let getType = () => numberCreated % 2 ? "btn-primary" : "btn-secondary";
+      return {
+        create: () => {
+          numberCreated = numberCreated + 1;
+          let button = document.createElement("button");
+          button.className = "btn " + getType() + " m-1 d-block";
+          return button
+        }
+      }
+    })(); 
+
     
-    col.append(heading);
-    col.append(list);
-  
-    row.append(col)
+    let actions = orientationButton.create();
+    let convert = (value) => Math.round(value / questions.getNumberOfQuestions() * 100);
+    actions.innerText = "Actions: " + convert(communicationStyles.getAction()) + "%";
+    let process = orientationButton.create();
+    process.innerText = "Process: " + convert(communicationStyles.getProcess()) + "%";
+    let people = orientationButton.create();
+    people.innerText = "People: " + convert(communicationStyles.getPeople()) + "%";
+    let ideas = orientationButton.create();
+    ideas.innerText = "Ideas: " + convert(communicationStyles.getIdeas()) + "%";
+
+    let infoPanel = document.createElement("div");
+    infoPanel.className = "col-lg-6 col-sm-12";
+    
+    let clearDisplay = () => {
+      infoPanel.innerHTML = "";
+    }
+
+    let displayHeading = (heading) => {
+      let title = document.createElement("h2");
+      title.innerText = "Communicating with "+ heading +" orientated person";
+      infoPanel.append(title);
+    }
+
+    let addToList = (value) => {
+      let listItem = document.createElement("li");
+      listItem.innerHTML = value;
+      return listItem;
+    }
+
+    let displayAction = () => {
+      displayHeading("an Action");
+      let list = document.createElement("ul");
+      list.append(addToList("Focus on the results first (state the conclusion at the outset)"));
+      list.append(addToList("State your best recommendation (do not offer many alternatives)"));
+      list.append(addToList("Be as brief as possible"));
+      list.append(addToList("Emphasize the practicality of your ideas"));
+      list.append(addToList("Use visual aids"));
+      infoPanel.append(list);
+    }
+
+    let displayProcess = () => {
+      displayHeading("a Process");
+      let list = document.createElement("ul");
+      list.append(addToList("Be precise (state the facts)"));
+      list.append(addToList("Organize your presentation in a logical order:"));
+      let sublist = document.createElement("ul");
+      sublist.append(addToList("Background"));
+      sublist.append(addToList("Present situation"));
+      sublist.append(addToList("Outcome"));
+      list.append(sublist);
+      list.append(addToList("Break down your recommendations"));
+      list.append(addToList("Include options (consider alternatives) with pros and cons"));
+      list.append(addToList("Do not rush a process-oriented person"));
+      list.append(addToList("Outline your proposal (1, 2, 3...)"));
+      infoPanel.append(list);
+    }
+
+    let displayPeople = () => {
+      displayHeading("a People");
+      let list = document.createElement("ul");
+      list.append(addToList("Allow for small talk (do not start the discussion right away)"));
+      list.append(addToList("Stress the relationships between your proposal and the people concerned"));
+      list.append(addToList("Show how the idea worked well in the past"));
+      list.append(addToList("Indicate support from well-respected people"));
+      list.append(addToList("Use an informal writing style"));
+      infoPanel.append(list);
+    }
+
+    let displayIdea = () => {
+      displayHeading("an Idea");
+      let list = document.createElement("ul");
+      list.append(addToList("Allow enough time for discussion"));
+      list.append(addToList("Do not get impatient when he or she goes off on tangents"));
+      list.append(addToList("In your opening, try to relate the discussed topic to a broader concept or idea (in other words, be conceptual"))
+      list.append(addToList("Stress the uniqueness of the idea or topic at hand"));
+      list.append(addToList("Emphasize future value or relate the impact of the idea to the future"));
+      list.append(addToList("If writing to an idea-oriented person, try to stress the key concepts that underlie your proposal or recommendation at the outset. Start with an overall statement and work toward the more particular."));
+      infoPanel.append(list);
+    }
+
+    let infoDisplay = () => {
+      clearDisplay();
+      return {
+        action: () => displayAction(),
+        process: () => displayProcess(),
+        people: () => displayPeople(),
+        ideas: () => displayIdea()
+      }
+    }
+
+    actions.addEventListener("click", () => infoDisplay().action());
+    process.addEventListener("click", () => infoDisplay().process());
+    people.addEventListener("click", () => infoDisplay().people());
+    ideas.addEventListener("click", () => infoDisplay().ideas());
+
+    let canvas = document.createElement("canvas");
+    canvas.width = "100";
+    let chart = new Chart(canvas, {
+      type: "pie",
+      data: {
+        labels: ["Action", "Process", "People", "Idea"],
+        datasets: [{
+          backgroundColor: ["#ffC000", "#4472C4", "#ed7d31", "#a5a5a5"],
+          data: [
+            convert(communicationStyles.getAction()),
+            convert(communicationStyles.getProcess()),
+            convert(communicationStyles.getPeople()),
+            convert(communicationStyles.getIdeas())
+          ]
+        }]
+      }
+    })
+
+
+    results.append(heading);
+    results.append(character);
+    results.append(actions);
+    results.append(process);
+    results.append(people);
+    results.append(ideas);
+    results.append(canvas);
+
+    row.append(results);
+    row.append(infoPanel);
   }
 
   let createButtons = () => {
     let numberOfOptions = questions.getNumberOfOptions();
     if (numberOfOptions > 0) {
-      for (let i = 0; i < numberOfOptions; i++){
+      for (let i = 0; i < numberOfOptions; i++) {
         createButton(i);
       }
       createProgressBar();
@@ -260,16 +424,5 @@ let choices = (() => {
     }
   }
   createButtons();
-  
+
 })()
-
-
-
-
-
-
-
-
-
-
-
